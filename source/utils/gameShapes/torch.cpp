5f814db263a3939a4b12222fba4ebcce0077179b
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-03-03 20:00:43 -0300

    Fixed torch flame.

--- source/utils/gameShapes/torch.cpp
+++ source/utils/gameShapes/torch.cpp
@@ -45,7 +45,7 @@ void Torch::display(float dt, std::shared_ptr<ControllerData> controller) {
     glPushMatrix();
         glPushMatrix();
             this->flame->translate();
-            glTranslatef(0, block_size/3 + flame_size/2, -block_size/2);
+            glTranslatef(flame_size, block_size/3+flame_size/2, -block_size/2);
             glScalef(flame_size, flame_size, flame_size);
             glRotatef(controller->to_rotate.x, -1, 0, 0);
             glRotatef(controller->to_rotate.y, 0, -1, 0);
